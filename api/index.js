01d261355298a3a4a8e948f7cece827336bfe76b
Ayyoubez <ayyoubez@gmail.com>
2024-09-16 16:49:52 +0100

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,25 +1,23 @@
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("connected to database")
-}).catch((err)=>{
-    console.log("Failed",err);
-});
+app.use(express.json());
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("Failed", err);
+  });
 
-app.use("/api/user", userRouter)
-
-
-
-
-
-
-
-
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("server is running on port 3000 !!!");
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,17 +4,17 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, "Username is required"],
       unique: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "email is required"],
       unique: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
     },
   },
   { timestamps: true }
