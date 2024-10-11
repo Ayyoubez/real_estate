a9e2690acc839c1988200e1c6281cc210fb6d24e
Ayyoubez <ayyoubez@gmail.com>
2024-10-11 10:24:26 +0100

    fix updating bug appeared on Postman

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,10 +4,10 @@ import jwt from "jsonwebtoken";
 export const verfiyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(errorHandler(401, "Unauthorised"));
-  jwt.verify(token.process.env.JWT_SECRET),
+  jwt.verify(token,process.env.JWT_SECRET,
     (err, user) => {
       if (err) return next(errorHandler(403, "Forbidden"));
       req.user = user;
       next();
-    };
+    });
 };
