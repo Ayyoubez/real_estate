00afc66a4d8bdb7efdd6d4142e23d78beae05b3b
Ayyoubez <ayyoubez@gmail.com>
2024-10-11 12:06:02 +0100

    complete user profile update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,11 @@
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
+import {
+  updateUserSuccess,
+  updateUserStart,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 import { app } from "../firebase";
 import {
   getDownloadURL,
@@ -9,15 +15,13 @@ import {
 } from "firebase/storage";
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-
-  console.log(formData);
-  console.log(fileUploadError);
-  console.log(filePerc);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
@@ -47,11 +51,37 @@ export default function Profile() {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+  // console.log(formData);
 
   return (
     <div className="p-3  max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           onChange={(e) => setFile(e.target.files[0])}
@@ -67,7 +97,9 @@ export default function Profile() {
         />
         <p className="text-small self-center">
           {fileUploadError ? (
-            <span className="text-red-700">Error image Loading (Image must be less than 2MB)</span>
+            <span className="text-red-700">
+              Error image Loading (Image must be less than 2MB)
+            </span>
           ) : filePerc > 0 && filePerc < 100 ? (
             <span className="text-slate-700">{`Uploading ${filePerc} %`}</span>
           ) : filePerc === 100 ? (
@@ -78,30 +110,42 @@ export default function Profile() {
         </p>
         <input
           id="username"
+          onChange={handleChange}
+          defaultValue={currentUser.username}
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
         />
         <input
           id="email"
+          onChange={handleChange}
+          defaultValue={currentUser.email}
           type="text"
           placeholder="email"
           className="border p-3 rounded-lg"
         />
         <input
           id="password"
-          type="text"
+          onChange={handleChange}
+          type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
         />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading... " : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User Updated successfully " : ""}
+      </p>
     </div>
   );
 }
--- client/src/redux/user/userSlice.jsx
+++ client/src/redux/user/userSlice.jsx
@@ -15,14 +15,26 @@ const userSlice = createSlice({
     },
     signInSuccess: (state, action) => {
       console.log("User signed in:", action.payload);
-      state.currentUser = action.payload,
+      (state.currentUser = action.payload),
         (state.error = null),
         (state.loading = false);
     },
     signInFailure: (state, action) => {
       (state.error = action.payload), (state.loading = false);
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
-export const {signInFailure, signInStart, signInSuccess} = userSlice.actions;
-export default userSlice.reducer
+export const { signInFailure, signInStart, signInSuccess , updateUserFailure, updateUserStart, updateUserSuccess} = userSlice.actions;
+export default userSlice.reducer;
