106ab1bae025568c62ebf692874a4d830274c9e4
Ayyoubez <ayyoubez@gmail.com>
2024-10-12 11:08:34 +0100

    add create listing api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {test, updateUser, deleteUser} from "../controllers/user.controller.js"
-import { verfiyToken } from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.post("/update/:id", verfiyToken,updateUser)
-router.delete("/delete/:id", verfiyToken, deleteUser);
+router.post("/update/:id", verifyToken,updateUser)
+router.delete("/delete/:id", verifyToken, deleteUser);
 export default router;
