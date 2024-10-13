3e0896dfbc17cb940012602aa3bc74e03f795460
Ayyoubez <ayyoubez@gmail.com>
2024-10-13 12:05:47 +0100

    Complete listing page functionnality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,7 +5,7 @@ export const createListing = async (req,res, next) =>{
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing)
     } catch (error) {
-        next()
+        next(error)
     }
 
 }
