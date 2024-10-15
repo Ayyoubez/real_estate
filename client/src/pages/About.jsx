a70c99b430c703fee4fd70d4e10f5e27b7bc6ab5
Ayyoubez <ayyoubez@gmail.com>
2024-10-15 11:58:20 +0100

    completed About page and Updated index.html

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,42 @@
 
 export default function About() {
   return (
-    <div>
-    About
+    <div className="max-w-6xl py-20 px-4 mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        About Kech Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu erat
+        tempor, sodales enim et, efficitur ligula. Curabitur molestie est non
+        venenatis hendrerit. Sed ac lacus gravida, lobortis mauris et, blandit
+        arcu. Sed tellus quam, convallis nec aliquam ac, sollicitudin sed ex.
+        Integer eu sapien sed lectus molestie eleifend. Praesent dictum eleifend
+        commodo. Vivamus volutpat nisl sodales posuere iaculis. Vivamus
+        pellentesque venenatis euismod. Pellentesque ultricies arcu urna, ut
+        aliquam velit euismod vitae. Proin varius magna non nulla porta
+        molestie. Donec tristique vehicula ex nec congue. Cras sodales nisl
+        lorem, ut hendrerit turpis congue at. Morbi sit amet malesuada arcu, a
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu erat
+        tempor, sodales enim et, efficitur ligula. Curabitur molestie est non
+        venenatis hendrerit. Sed ac lacus gravida, lobortis mauris et, blandit
+        arcu. Sed tellus quam, convallis nec aliquam ac, sollicitudin sed ex.
+        Integer eu sapien sed lectus molestie eleifend. Praesent dictum eleifend
+        commodo. Vivamus volutpat nisl sodales posuere iaculis. Vivamus
+        pellentesque venenatis euismod. Pellentesque ultricies arcu urna, ut
+        aliquam velit euismod vitae. Proin varius magna non nulla porta
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu erat
+        tempor, sodales enim et, efficitur ligula. Curabitur molestie est non
+        venenatis hendrerit. Sed ac lacus gravida, lobortis mauris et, blandit
+        arcu. Sed tellus quam, convallis nec aliquam ac, sollicitudin sed ex.
+        Integer eu sapien sed lectus molestie eleifend. Praesent dictum eleifend
+        commodo. Vivamus volutpat nisl sodales posuere iaculis. Vivamus
+        pellentesque venenatis euismod. Pellentesque ultricies arcu urna, ut
+        aliquam velit euismod vitae. Proin varius magna non nulla porta
+      </p>
     </div>
-  )
+  );
 }
